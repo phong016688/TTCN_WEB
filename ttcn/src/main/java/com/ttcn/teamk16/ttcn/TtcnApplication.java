0550522eda5cd3c8f351c1c3a87a6c1904544d8c
phong016688 <vophongqt1998@gmail.com>
2020-11-11 17:41:37 +0700

    initial

---
+++ ttcn/src/main/java/com/ttcn/teamk16/ttcn/TtcnApplication.java
@@ -0,0 +1,13 @@
+package com.ttcn.teamk16.ttcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtcnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtcnApplication.class, args);
+    }
+
+}
---
+++ ttcn/src/test/java/com/ttcn/teamk16/ttcn/TtcnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcn.teamk16.ttcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtcnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
