2aecf6620410056ba037839f73e031b2b83c7d2b
phong016688 <vophongqt1998@gmail.com>
2020-12-21 22:17:26 +0700

    21.12.2020 commit app

--- ttcn/src/main/java/com/ttcn/teamk16/utils/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package storysflower.com.storysflower.utils;
-
-import java.sql.Time;
-
-public class TimeUtil {
-    public static Time convertFromStringToTimeSql(String time) {
-        return Time.valueOf(time);
-    }
-}
